e52f98593367482b3c8c3e663e4599f7fec9cd42
noeyos-p <krapnoeyos@gmail.com>
2025-09-05 17:06:53 +0900

    빌더추가 오타수정

--- src/main/java/com/project/tea/entity/UserEntity.java
+++ src/main/java/com/project/tea/entity/UserEntity.java
@@ -2,6 +2,7 @@ package com.project.tea.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor  // 기본 생성자 추가
 @AllArgsConstructor // 전체 필드 생성자 추가 (선택 사항)
 @Entity
+@Builder
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
