b80d605051549929414aad3de89f8f9f32fc1b55
ravvitkim <mirkim6086@naver.com>
2025-09-05 12:14:47 +0900

    무드서비스 코드 설명 주석달았음

--- src/main/java/com/project/tea/repository/MoodCheckRepository.java
+++ src/main/java/com/project/tea/repository/MoodCheckRepository.java
@@ -2,15 +2,13 @@ package com.project.tea.repository;
 
 import com.project.tea.entity.MoodCheckEntity;
 import com.project.tea.entity.MoodEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface MoodCheckRepository {
+public interface MoodCheckRepository extends JpaRepository<MoodCheckEntity, Long> {
     // 특정 Mood에 연결된 체크리스트 조회
     List<MoodCheckEntity> findByMood(MoodEntity mood);
-
-    Optional<Object> findById(Long id);
 }
