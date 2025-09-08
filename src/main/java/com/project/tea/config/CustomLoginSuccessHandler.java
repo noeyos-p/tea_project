cd90040899be5d385f270443f766479a5364cdcd
ravvitkim <mirkim6086@naver.com>
2025-09-08 16:33:14 +0900

    로그인 기능구현 완료

---
+++ src/main/java/com/project/tea/config/CustomLoginSuccessHandler.java
@@ -0,0 +1,28 @@
+package com.project.tea.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication)
+            throws IOException, ServletException {
+
+        String userEmail = authentication.getName(); // 로그인한 사용자 이메일
+
+        if ("admin@admin.com".equalsIgnoreCase(userEmail)) {
+            response.sendRedirect("/admin");
+        } else {
+            response.sendRedirect("/main");
+        }
+    }
+}
--- src/main/java/com/project/tea/repository/UserDataRepository.java
+++ src/main/java/com/project/tea/repository/UserDataRepository.java
@@ -1,6 +1,7 @@
 package com.project.tea.repository;
 
 import com.project.tea.entity.UserDataEntity;
+import com.project.tea.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,5 @@ public interface UserDataRepository extends JpaRepository<UserDataEntity, Long> 
     //전체기록 조회
     List<UserDataEntity> findByUserId(Long userId);
 
-    //최신기록 조회(메모)
-    Optional<UserDataEntity> findTopByUserIdOrderByDateDesc(Long userId);
+//    Optional<UserEntity> findByEmail(String email);
 }
