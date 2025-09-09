0fc717062bf4d96655dc1cee2a2daf1c3128bed7
noeyos-p <krapnoeyos@gmail.com>
2025-09-09 15:07:00 +0900

    로그인 회원가입 오류 수정 회원가입 css 오류 수정예정

--- src/main/java/com/project/tea/dto/ChooseDto.java
+++ src/main/java/com/project/tea/dto/ChooseDto.java
@@ -12,14 +12,14 @@ public class ChooseDto {
     private Long teaId;
     private String teaName;
     private Long count;
-    private String imagePath;  // ★ 추가
+//    private String imagePath;  // ★ 추가
 
     public static ChooseDto fromEntity(ChooseEntity e) {
         return ChooseDto.builder()
                 .teaId(e.getTea().getId())
                 .teaName(e.getTea().getName())
                 .count(e.getCount() == null ? 0L : e.getCount())
-                .imagePath(e.getTea().getImagePath())
+//                .imagePath(e.getTea().getImagePath())
                 .build();
     }
 }
