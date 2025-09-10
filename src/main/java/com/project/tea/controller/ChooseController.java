4b51b3959d0789d4c66472ffb71cdd65710cde1a
ravvitkim <shigbbang1234@gmail.com>
2025-09-10 14:28:39 +0900

    메모 페이지 템플릿 연결 작업중입니다

--- src/main/java/com/project/tea/controller/ChooseController.java
+++ src/main/java/com/project/tea/controller/ChooseController.java
@@ -15,7 +15,6 @@ import org.springframework.data.domain.Pageable;
 public class ChooseController {
 
     private final ChooseService chooseService;
-    private final UserDataService userDataService;
 
     // main
     @GetMapping("/")
@@ -40,44 +39,6 @@ public class ChooseController {
     }
 
 
-    // 메모 작성 페이지
-    @GetMapping("/memo/new")
-    public String showMemoCreateForm(Model model) {
-        model.addAttribute("userData", new UserDataEntity());
-        return "memo-create";
-    }
-    // 메모 수정 페이지
-    @GetMapping("/memo/edit/{id}")
-    public String showMemoEditForm(@PathVariable("id") Long userDataId, Model model) {
-        UserDataEntity data = userDataService.getUserDataById(userDataId);
-        model.addAttribute("userData", data);
-        return "memo-edit";
-    }
-
-    //작성
-    @PostMapping("/memo")
-    public String createMemo(
-            @RequestParam Long userDataId,
-            @RequestParam String memo,
-            RedirectAttributes redirectAttributes
-    ) {
-        userDataService.saveMemo(userDataId, memo);
-        redirectAttributes.addFlashAttribute("message", "메모가 작성되었습니다.");
-        return "redirect:/memo/new";
-    }
-
-
-    // 수정
-    @PostMapping("/memo/{id}")
-    public String updateMemo(
-            @PathVariable Long id,
-            @RequestParam String memo,
-            RedirectAttributes redirectAttributes
-    ) {
-        userDataService.saveMemo(id, memo);
-        redirectAttributes.addFlashAttribute("message", "메모가 수정되었습니다.");
-        return "redirect:/memo/edit/{id}";
-    }
 
 
 }
